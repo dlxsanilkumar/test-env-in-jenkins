0455b35e40ba313aaa310b2c76c9e5b40e21c0e8
Anil Kumar N <41360061+dlxsanilkumar@users.noreply.github.com>
2021-12-09 23:08:27 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,8 +5,10 @@ db_hostname = 'HOSTNAME'
 db_name = 'DATABASE'
 db_port = 'PORT'
 db_user = 'USER'
+jenkins_build_number = 'BUILD_NUMBER'
 
 print('Database hostname: ' + os.getenv(db_hostname))
 print('Database port: ' + os.getenv(db_port))
 print('Database name: ' + os.getenv(db_name))
 print('Database user: ' + os.getenv(db_user))
+print('Jenkins BUILD_NUMBER: ' + os.getenv(jenkins_build_number))
